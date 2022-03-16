5f7bb5178b30f811f0a1a5648b32cf5bbc425e76
Macedonian-MKD <macedonian.macedon@yahoo.com>
2022-03-16 21:10:51 +0100

    asd

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '497ea9b7-1e20-421b-a83e-e5c57f78538f';
 export const MODAL_CLOSE_SEC = 2.5;
